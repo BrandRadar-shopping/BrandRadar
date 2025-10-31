d3c8e7c2adf581a2074ef9841fc52a59b0f815e4
BrandRadar-shopping <andersgylder@gmail.com>
2025-10-31 11:39:08 +0100

    Update product.js

--- product.js
+++ product.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("buy-link").href = product.url;
 
   const ratingEl = document.getElementById("product-rating");
-  ratingEl.textContent = product.rating ? `⭐ ${product.rating}/5` : "";
+  ratingEl.textContent = product.rating ? `⭐ ${product.rating}: "";
 
   // Rabatt
   const discountEl = document.getElementById("product-discount");
