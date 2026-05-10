6b6345764ecbb73e9709dff4d401ecf92598956e
BrandRadar-shopping <andersgylder@gmail.com>
2026-05-10 14:27:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -141,8 +141,8 @@ const BR_MENU_ICON_ASSETS = {
 
   "Slip-ins": "icons/menu/shoes/woman-slip-in.svg",
 
-  "Sandaler / Åpne sko": "icons/menu/shoes/woman-sandal.svg",
-  "Sandaler / Badesko": "icons/menu/shoes/sandals.svg",
+  "Sandaler / Åpne sko": "icons/menu/shoes/sandals.svg",
+  "Sandaler / Badesko": "icons/menu/shoes/womens-sandal.svg",
 
   "Tøfler": "icons/menu/shoes/slippers.svg",
   "Sportssko": "icons/menu/shoes/running-shoes.svg",
