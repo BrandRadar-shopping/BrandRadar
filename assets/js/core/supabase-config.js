52f2e0adde1a0ee766bc9a460dac0ca29a50a4ee
BrandRadar-shopping <andersgylder@gmail.com>
2026-05-12 18:26:16 +0200

    Update supabase-config.js

--- assets/js/core/supabase-config.js
+++ assets/js/core/supabase-config.js
@@ -2,3 +2,6 @@ window.SUPABASE_CONFIG = {
   url: "https://eifrfwtkyrirrmaupfij.supabase.co",
   anonKey: "sb_publishable_4t_wIAT5FjDklllDJtYkaA_IlEkbxn2"
 };
+
+window.BRANDRADAR_SUPABASE_URL = window.SUPABASE_CONFIG.url;
+window.BRANDRADAR_SUPABASE_ANON_KEY = window.SUPABASE_CONFIG.anonKey;
